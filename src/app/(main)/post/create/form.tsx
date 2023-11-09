7ec722123a337e9fefb0a31a125b9a59fc59237c
hhkhoa96 <122526568+hhkhoa96@users.noreply.github.com>
2023-11-09 20:16:54 +0700

    Update navbar ui

--- src/app/(main)/post/create/form.tsx
+++ src/app/(main)/post/create/form.tsx
@@ -1,71 +1,105 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import styles from './styles.module.css';
-import classNames from 'classnames';
-import { useRouter } from 'next/navigation';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import styles from "./styles.module.css";
+import classNames from "classnames";
+import { useRouter } from "next/navigation";
 
 interface FormValue {
   title: string;
-  description: string
+  description: string;
 }
 
-
 export default function CreatePostForm() {
-  const [submitting, setSubmitting] = useState(false)
+  const [submitting, setSubmitting] = useState(false);
   const router = useRouter();
-  const { register, handleSubmit, formState: { errors, isValid } } = useForm<FormValue>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormValue>();
 
-  const submit = handleSubmit(form => {
+  const submit = handleSubmit((form) => {
     const formData = new FormData();
-    formData.append('title', form.title)
-    formData.append('description', form.description)
+    formData.append("title", form.title);
+    formData.append("description", form.description);
 
-    setSubmitting(true)
-    fetch('/api/post/create', {
-      method: 'POST',
+    setSubmitting(true);
+    fetch("/api/post/create", {
+      method: "POST",
       body: formData,
-    }).then(res => res.json())
-      .then(res => {
-        router.push('/post/1')
-      }).catch(error => {
-        console.log({ error })
-      }).finally(() => {
-        setSubmitting(false);
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        router.push("/post/1");
       })
-  })
+      .catch((error) => {
+        console.log({ error });
+      })
+      .finally(() => {
+        setSubmitting(false);
+      });
+  });
 
   return (
     <form onSubmit={submit} className={styles.form}>
-      <label className={styles.formLabel} htmlFor="title">Post title</label>
+      <label className={styles.formLabel} htmlFor="title">
+        Post title
+      </label>
       <div className="form-group">
-        <input className="form-control" id="title" {...register('title', {
-          required: true
-        })} />
-        <p className={classNames(styles.errorMessage, {
-          [styles.error]: errors.title
-        })}>* Required</p>
+        <input
+          className="form-control"
+          id="title"
+          {...register("title", {
+            required: true,
+          })}
+        />
+        <p
+          className={classNames(styles.errorMessage, {
+            [styles.error]: errors.title,
+          })}
+        >
+          * Required
+        </p>
       </div>
 
-      <label className={styles.formLabel} htmlFor="description">Post description</label>
+      <label className={styles.formLabel} htmlFor="description">
+        Post description
+      </label>
       <div className="form-group">
-        <input className="form-control" id="description" {...register('description', {
-          required: true
-        })} />
-        <p className={classNames(styles.errorMessage, {
-          [styles.error]: errors.description
-        })}>* Required</p>
+        <input
+          className="form-control"
+          id="description"
+          {...register("description", {
+            required: true,
+          })}
+        />
+        <p
+          className={classNames(styles.errorMessage, {
+            [styles.error]: errors.description,
+          })}
+        >
+          * Required
+        </p>
       </div>
 
-      <label className={styles.formLabel} htmlFor="description">Select a thumbnail</label>
       <div className="form-group">
-        <input type="file" className="form-control" id="description" />
+        <label className={styles.formLabel} htmlFor="description">
+          Select a thumbnail
+        </label>
+        <input type="file" id="inputFile" />
       </div>
 
       <div className={styles.formFooter}>
-        <button type="submit" className={classNames('btn', 'btn-default', styles.submit)} disabled={submitting || !isValid}>Submit</button>
+        <button
+          type="submit"
+          className={classNames("btn", "btn-default", styles.submit)}
+          disabled={submitting || !isValid}
+        >
+          Create Post
+        </button>
       </div>
     </form>
-  )
+  );
 }
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,50 +1,33 @@
 import { authorization } from "@/app/actions";
 import Link from "next/link";
-import CreatePost from "./login-button";
 import LoginButton from "./login-button";
-
+import styles from "./styles.module.css";
 
 export default async function Navbar() {
   const { loggedIn } = await authorization();
 
   return (
-    <nav className="navbar navbar-default navbar-custom navbar-fixed-top">
-      <div className="container-fluid">
-        <div className="navbar-header page-scroll">
-          <button
-            type="button"
-            className="navbar-toggle"
-            data-toggle="collapse"
-            data-target="#bs-example-navbar-collapse-1"
-          >
-            <span className="sr-only">Toggle navigation</span>
-            Menu <i className="fa fa-bars"></i>
-          </button>
-          <Link className="navbar-brand" href="/">
-            Start Bootstrap
-          </Link>
-        </div>
-
-        <div
-          id="bs-example-navbar-collapse-1"
-        >
-          <ul className="nav navbar-nav navbar-right">
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">About</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contact</Link>
-            </li>
-            <li>
-              <Link href="/post/create">Create Post</Link>
-            </li>
-            <LoginButton loggedIn={loggedIn} />
-          </ul>
-        </div>
+    <nav className={styles.navbar}>
+      <div className="left">
+        <Link href="/">Beautiful Website</Link>
+      </div>
+      <div className="right">
+        <ul className="list">
+          <li className="list-item">
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link href="/post/create">Create Post</Link>
+          </li>
+          <LoginButton loggedIn={loggedIn} />
+        </ul>
       </div>
     </nav>
-  )
+  );
 }
